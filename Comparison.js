67b9489cc07075203bcfc6e51a15acbc8b6aa7c2
fathcode99 <fath970106@gmail.com>
2022-07-15 10:17:16 +0700

    Day 2

---
+++ Comparison.js
@@ -0,0 +1,14 @@
+//Boolean 
+
+console.log(5 > 2)
+console.log(5 < 2)
+console.log(5 >= 2)
+console.log(2 >= 5)
+console.log(5 == '5')
+console.log(5 === '5')
+console.log(5 != 5)
+console.log(5 !== '5')
+console.log(1 < 2 && 2 > 3)
+console.log(1 < 4 || 3 > 4)
+console.log(null == undefined)
+console.log(null === undefined)
