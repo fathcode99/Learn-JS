8b4c11c1f3871fd0721fc6e8f5f3d7502c1ef21e
fathcode99 <fath970106@gmail.com>
2022-07-15 10:54:47 +0700

    Day_3

---
+++ IMT.js
@@ -0,0 +1,18 @@
+let massa = 150,
+    tinggi = 1.78,
+    imt = massa / tinggi ** 2
+    
+
+if (imt < 18.5 ) {
+    notif = `Berat badan Kurang`
+} else if (imt > 18.5 && imt < 24.9) {
+    notif = `Berat badan ideal`
+} else if (imt > 25.0 && imt < 29.9) {
+    notif = `Berat Badan Berlebih`
+} else if (imt > 30.0 && imt < 39.9) {
+    notif = `Berat Badan Sangat berlebih`
+} else if (imt > 39.9) {
+    notif = `Obesitas Bos !`
+}
+
+console.log(notif)
