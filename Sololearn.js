dc7fb28b8cb3142271761eddffae8229a69fc644
fathcode99 <fath970106@gmail.com>
2022-07-27 08:56:06 +0700

    Day 11

--- Sololearn.js
+++ Sololearn.js
@@ -1,30 +1,68 @@
-// snail
-// var x = 42
-// var y = 0
-// for (; x > 0;x--) {
-//     x-= 7
-//     y++
-//     for (; x > 0;x-=7) {
-//         x+=2
-//         if (x < 7) {
-//             break
-//         }
-//         y++
-//     }
-// }
-// console.log(y)
+// // snail
+// // var x = 42
+// // var y = 0
+// // for (; x > 0;x--) {
+// //     x-= 7
+// //     y++
+// //     for (; x > 0;x-=7) {
+// //         x+=2
+// //         if (x < 7) {
+// //             break
+// //         }
+// //         y++
+// //     }
+// // }
+// // console.log(y)
 
-// function addnumber(a, b) {
-//     var c = a+b;
-//     return c;
-// }
-// console.log(addnumber(40, 2))
+// // function addnumber(a, b) {
+// //     var c = a+b;
+// //     return c;
+// // }
+// // console.log(addnumber(40, 2))
 
-var amount = 100
-var rate = 1.1
-function convert(amount, rate) {
-    convert = amount*rate
-    return convert
+// var amount = 100
+// var rate = 1.1
+// function convert(amount, rate) {
+//     convert = amount*rate
+//     return convert
+// }
+
+// console.log(convert(amount, rate));
+
+// function contact(name, number) {
+//     this.name = name;
+//     this.number = number;
+// }
+
+// var a = new contact("David", 12345);
+// var b = new contact("Amy", 987654321);
+// // la.name + `: ` + a.number);
+
+// print(a.name + `: ` + a.number);
+
+// console.log(a)
+// var date = new Date()
+// var date = new Date();
+// console.log(date.getMinutes())
+
+function main() {
+    var increase = 9;
+    var prices = [98.99, 15.2, 20, 1026];
+    
+    var count = new Array();
+    for (i=0; i < prices.length; i++) {
+        count[i] = prices[i] + increase
+    }
+    console.log(count)
 }
+// main()
 
-console.log(convert(amount, rate));
+// let greeting = () => {
+//     let name1 = 'Anton '
+//     let name2 = 'Andi'
+//     let namaLengkap = name1 + name2
+
+//     return namaLengkap
+// }
+
+// console.log(greeting())
---
+++ databse.js
@@ -0,0 +1,40 @@
+let products = [
+    {
+        id : 1234567890,
+        kate : "Fast Food",
+        nama : "Noodle",
+        harga : 3500,
+        stok :9
+    },
+    {
+        id : 1234567891,
+        kate : "Electronic",
+        nama : "Headset",
+        harga : 300000,
+        stok : 8
+    },
+    {
+        id : 1234567892,
+        kate : "Cloth",
+        nama : "Hoodie",
+        harga : 150000,
+        stok : 7
+    },
+    {
+        id : 1234567893,
+        kate : "Fruit",
+        nama : "Apple",
+        harga : 10000,
+        stok : 8
+    }
+]
+
+class Product {
+    constructor(id, kate, nama, harga, stok) {
+        this.id = id
+        this.kate = kate
+        this.nama = nama
+        this.harga = harga
+        this.stok = stok
+    }
+}
